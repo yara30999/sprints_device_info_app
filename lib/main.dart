0ec427a8d1e935ba50360a53055a87e3ff6be5d9
yara30999 <yaranaserelniny@gmail.com>
2025-02-06 10:28:25 +0200

    read device info.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  String deviceModel = 'Unknown';
+  String osVersion = 'Unknown';
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _getDeviceInfo();
+  }
+
+  Future<void> _getDeviceInfo() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+    if (Theme.of(context).platform == TargetPlatform.android) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      setState(() {
+        deviceModel = androidInfo.model;
+        osVersion = androidInfo.version.release;
+      });
+    } else if (Theme.of(context).platform == TargetPlatform.iOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      setState(() {
+        deviceModel = iosInfo.model;
+        osVersion = iosInfo.systemVersion;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Access Device Features App'),
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.png"),
+            fit: BoxFit.cover, // Adjust image fit
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Device Model: $deviceModel',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              SizedBox(height: 50),
+              Text(
+                'OS Version: $osVersion',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
